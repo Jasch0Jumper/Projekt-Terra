de61d3e8150a2c561be371b566614d584b3a9df7
Jasch Dhungel <53111523+Jasch0Jumper@users.noreply.github.com>
2023-10-04 22:37:43 +0200

    worked on proton attraction

--- Assets/_Project/Scripts/ProtonBehaviour.cs
+++ Assets/_Project/Scripts/ProtonBehaviour.cs
@@ -1,7 +1,49 @@
+using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Serialization;
+
 namespace Terra
 {
-    public class ProtonBehaviour
+    public class ProtonBehaviour : MonoBehaviour
     {
-	
+        [SerializeField] [Range(0f, 10000f)] private float _attractionForce = 1f;
+        
+        private static List<ProtonBehaviour> _protons;
+        
+        [HideInInspector] [NotNull] public List<ProtonBehaviour> connectedProtons = new();
+
+        private void Awake() => _protons ??= new List<ProtonBehaviour>();
+        private void OnEnable() => _protons.Add(this);
+        private void OnDisable() => _protons.Remove(this);
+
+        private void FixedUpdate()
+        {
+            var positionDelta = Vector3.zero;
+            foreach (var proton in _protons)
+            {
+                if (proton == this) continue;
+                if (connectedProtons.Contains(proton)) continue;
+
+                var otherPosition = proton.transform.position;
+                var thisPosition = transform.position;
+                var direction = (otherPosition - thisPosition).normalized;
+
+                var forceBase = (float)Math.Pow(1.5, -(otherPosition - thisPosition).sqrMagnitude);
+                var forceMultiplier = _attractionForce * proton.connectedProtons.Count;
+                
+                positionDelta += direction * (forceBase * forceMultiplier);
+            }
+            transform.position += positionDelta * Time.fixedDeltaTime;
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.TryGetComponent(out ProtonBehaviour proton))
+            {
+                connectedProtons.Add(proton);
+            }
+        }
     }
 }
