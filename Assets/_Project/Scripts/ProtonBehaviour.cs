2e86025fb792851ac2a62be3ea1938f1f005ad01
Jasch Dhungel <53111523+Jasch0Jumper@users.noreply.github.com>
2023-10-05 17:16:14 +0200

    changed proton attraction behaviour

--- Assets/_Project/Scripts/ProtonBehaviour.cs
+++ Assets/_Project/Scripts/ProtonBehaviour.cs
@@ -2,13 +2,12 @@ using System;
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Terra
 {
     public class ProtonBehaviour : MonoBehaviour
     {
-        [SerializeField] [Range(0f, 10000f)] private float _attractionForce = 1f;
+        [SerializeField] [Range(0f, 10f)] private float _attractionForce = 1f;
         
         private static List<ProtonBehaviour> _protons;
         
@@ -30,8 +29,10 @@ namespace Terra
                 var thisPosition = transform.position;
                 var direction = (otherPosition - thisPosition).normalized;
 
-                var forceBase = (float)Math.Pow(1.5, -(otherPosition - thisPosition).sqrMagnitude);
-                var forceMultiplier = _attractionForce * proton.connectedProtons.Count;
+                var forceBase = (float)Math.Pow(0.9, 1.5 * (-Vector3.Distance(otherPosition, thisPosition) - 20));
+                var forceMultiplier = _attractionForce * (proton.connectedProtons.Count == 0 ? 1f : proton.connectedProtons.Count) / 10;
+
+                if (forceMultiplier == 0f) forceMultiplier = 1f;
                 
                 positionDelta += direction * (forceBase * forceMultiplier);
             }
