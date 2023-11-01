894b3a1c89f6a36e641c01422d4443b16c0de9df
Jasch Dhungel <53111523+Jasch0Jumper@users.noreply.github.com>
2023-11-01 20:28:29 +0100

    Update main.rs

--- Bevy/sanomic/src/main.rs
+++ Bevy/sanomic/src/main.rs
@@ -1,9 +1,20 @@
-// fn main() {
-//     println!("Hello, world!");
-// }
-
-use bevy::prelude::*;
 
 fn main() {
-    App::new().run();
+    let my_var = 123.34;
+
+    println!("Hello, world!");
+    println!("I'm a Rustacean!");
+    println!(my_var);
 }
+
+// use bevy::prelude::*;
+
+// fn main() {
+//    App::new()
+//        .add_systems(Update, hello_world_system)
+//        .run();
+// }
+
+// fn hello_world_system() {
+//    println!("hello world");
+// }
